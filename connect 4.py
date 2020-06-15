8ab9988ff45d7228b3a20456c517b71359efc857
RobertSalmonn <robertsalmon03@gmail.com>
2020-06-15 11:33:33 +0100

    debugging 1

--- connect 4.py
+++ connect 4.py
@@ -5,7 +5,10 @@ wn.tracer(0)
 wn.bgcolor("blue")
 wn.setup(width=600, height=600)
 
+game_over="no"
 turn="yellow"
+counter=0
+
 v_turn=1
 board=[[0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
@@ -43,6 +46,7 @@ class Piece (turtle.Turtle):
     def __init__(self):
         global turn
         global v_turn
+        
         if turn=="red":
             turn="yellow"
             v_turn=2
@@ -61,6 +65,7 @@ class Piece (turtle.Turtle):
         pieces.append(self)
 
     def animate (self):
+        global game_over
         
         
         y=self.ycor()
@@ -79,29 +84,38 @@ class Piece (turtle.Turtle):
                 y-=60
                 self.sety(y)
 
+        if counter==42:
+            game_over="yes"
+            
+            tie()
+
         
         for i in range (4):
             for j in range (6):
                 if board[j][i]==v_turn and board[j][i+1]==v_turn and board[j][i+2]==v_turn and board[j][i+3]==v_turn:
+                    game_over="yes"
+                    
                     
                     win(turn)
 
         for i in range (7):
             for j in range (3):
                 if board[j][i]==v_turn and board[j+1][i]==v_turn and board[j+2][i]==v_turn and board[j+3][i]==v_turn:
-                    
+                    game_over="yes"
                     win(turn)
         for i in range (4):
             for j in range (3):
                 if board[j][i]==v_turn and board[j+1][i+1]==v_turn and board[j+2][i+2]==v_turn and board[j+3][i+3]==v_turn:
-                    
+                    game_over="yes"
+
                     win(turn)
 
         for i in range (3, 7):
             for j in range (3):
                 if board[j][i]==v_turn and board[j+1][i-1]==v_turn and board[j+2][i-2]==v_turn and board[j+3][i-3]==v_turn:
-                    
+                    game_over="yes"
                     win(turn)
+        
                     
 
                 
@@ -115,32 +129,64 @@ class Piece (turtle.Turtle):
 
 
 def dropzone(x, y):
-    
+    global game_over
+    global turn
+    global counter
     global column
-    piece=Piece()
     
-    if x>-210 and x<-150:
-        piece.goto(-180, 150)
-        column=0
-    elif x>-150 and x<-90:
-        piece.goto(-120, 150)
-        column=1
-    elif x>-90 and x<-30:
-        piece.goto(-60, 150)
-        column=2
-    elif x>-30 and x<30:
-        piece.goto(0, 150)
-        column=3
-    elif x>30 and x<90:
-        piece.goto(60, 150)
-        column=4
-    elif x>90 and x<150:
-        piece.goto(120, 150)
-        column=5
-    elif x>150 and x<210:
-        piece.goto(180, 150)
-        column=6
-    piece.animate()
+    
+    
+    if game_over=="no":
+        piece=Piece()
+        if x>-210 and x<-150 and board[0][0]==0:
+            piece.goto(-180, 150)
+            column=0
+            counter+=1
+            piece.animate()
+        elif x>-150 and x<-90 and board[0][1]==0:
+            piece.goto(-120, 150)
+            column=1
+            counter+=1
+            piece.animate()
+        elif x>-90 and x<-30 and board[0][2]==0:
+            piece.goto(-60, 150)
+            column=2
+            counter+=1
+            piece.animate()
+        elif x>-30 and x<30 and board[0][3]==0:
+            piece.goto(0, 150)
+            column=3
+            counter+=1
+            piece.animate()
+        elif x>30 and x<90 and board[0][4]==0:
+            piece.goto(60, 150)
+            column=4
+            counter+=1
+            piece.animate()
+        elif x>90 and x<150 and board[0][5]==0:
+            piece.goto(120, 150)
+            column=5
+            counter+=1
+            piece.animate()
+        elif x>150 and x<210 and board[0][6]==0:
+            piece.goto(180, 150)
+            column=6
+            counter+=1
+            piece.animate()
+
+        else:
+            piece.goto(1000, 1000)
+            if turn=="red":
+                turn="yellow"
+            else:
+                turn="red"
+    else:
+        pass
+
+        
+
+        
+    
     
 
 
@@ -154,6 +200,12 @@ def win(team):
     pen.penup()
     pen.goto(0,200)
     pen.write(f"{team} wins!", align="center", font=("Calibri", 50))
+
+def tie():
+    pen.penup()
+    pen.goto(0,200)
+    pen.write(f"Draw!", align="center", font=("Calibri", 50))
+
     
 
 
